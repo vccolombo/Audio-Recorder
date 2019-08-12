9d2369eeaffd4f378da9ef0366ba86148546536c
vccolombo <victorcora98@gmail.com>
2019-08-12 11:07:25 -0300

    Add amplitude listener

--- app/src/main/java/com/github/vccolombo/audiorecorder/ui/recorder/RecorderViewModel.kt
+++ app/src/main/java/com/github/vccolombo/audiorecorder/ui/recorder/RecorderViewModel.kt
@@ -2,18 +2,20 @@ package com.github.vccolombo.audiorecorder.ui.recorder
 
 import android.media.MediaRecorder
 import android.os.Environment
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
 import timber.log.Timber
 import java.io.IOException
-import java.io.File.separator
-import android.os.Environment.DIRECTORY_DCIM
-import android.os.Environment.getExternalStorageDirectory
-import java.io.File
+import kotlin.coroutines.CoroutineContext
 
 
 class RecorderViewModel : ViewModel() {
+
+    private val job = Job()
+    private val coroutineContext: CoroutineContext = Dispatchers.Default + job
+    private val scope = CoroutineScope(coroutineContext)
+
     private var recording = MutableLiveData<Boolean>(false)
     private var recorder: MediaRecorder? = null
 
@@ -43,10 +45,29 @@ class RecorderViewModel : ViewModel() {
             start()
         }
 
+        // Audio amplitude for display
+        scope.launch {
+            repeat(1_000_000_000) {
+                delay(100L)
+                val amplitude = recorder?.maxAmplitude
+                Timber.d("""Repeat $it: $amplitude""")
+            }
+        }
+
     }
 
+    private fun pauseRecording() {
+        TODO()
+    }
+
+    // TODO: Change file name when finishing recording
+    // TODO: Check if killing the app while recording is safe
     private fun stopRecording() {
         Timber.d("Stop recording")
+
+        // Stop audio amplitude coroutine
+        job.cancel()
+
         recorder?.apply {
             stop()
             reset()
